779848cc6f7bdc828daa1f1112c5ac53fa4a5616
taijen <danielmadmonb@gmail.com>
2023-06-12 08:35:43 +0300

    working on gui

--- bin/tasker_gui/src/main.rs
+++ bin/tasker_gui/src/main.rs
@@ -1,12 +1,15 @@
-use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Application, Command, Theme, executor, Settings};
+use iced::theme::Container;
+use iced::widget::{button, column, text, Column, Row, row, container};
+use iced::{Alignment, Element, Application, Command, Theme, executor, Settings,theme, Renderer, Padding};
+use tasker_lib::taskerctl::Task;
 
-fn main() {
+
+fn main() -> iced::Result{
     let window_settings = iced::window::Settings{
-        size: (300,300),
+        size: (400,400),
         position: iced::window::Position::Centered,
-        min_size: Some((300,300)),
-        max_size: Some((300,300)),
+        min_size: Some((400,400)),
+        max_size: Some((400,400)),
         visible: true,
         resizable: false,
         decorations: true,
@@ -15,58 +18,96 @@ fn main() {
         icon: None,
         platform_specific: iced::window::PlatformSpecific,
     };
-    let mut custom_settings = Settings{
+    let custom_settings = Settings{
         id: Default::default(),
         window: window_settings,
         flags: Default::default(),
         default_font: Default::default(),
-        default_text_size: Default::default(),
+        default_text_size: 16.0,
         text_multithreading: Default::default(),
-        antialiasing: Default::default(),
+        antialiasing: false,
         exit_on_close_request: true,
         try_opengles_first: true,
     };
-    state::run(Settings::from(custom_settings));
+    State::run(Settings::from(custom_settings)) 
     
 }
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy,PartialEq)]
 enum MainMenu{
     CurrentTasks,
-    EditTasks,
-    Logs,
-    ServiceStatus,
-    About
+    ShowLogs,
+    SaveChanges,
+    DiscardAndExit
 }
-struct state{
-    do_nothing:bool
+#[derive(Clone,Copy)]
+struct State{
+    object:MainMenu
 }
-impl Application for state {
+impl Application for State {
     type Message = MainMenu;
     type Executor = executor::Default;
     type Flags = ();
     type Theme = Theme;
-    fn new(_flags: ()) -> (state,Command<Self::Message>) {
-        (Self { do_nothing: true },Command::none())
+    fn new(_flags: ()) -> (State,Command<Self::Message>) {
+        (Self { object: MainMenu::CurrentTasks },Command::none())
     }
 
     fn title(&self) -> String {
         String::from("Tasker")
     }
 
-    fn update(&mut self, message: Self::Message) -> Command<Self::Message>{
-        todo!()
+    fn update(&mut self, message: Self::Message)-> Command<MainMenu> {
+        match message {
+            MainMenu::CurrentTasks => {
+                self.object = MainMenu::CurrentTasks;
+                Command::none()
+            }
+            MainMenu::DiscardAndExit => {
+                Command::none()
+            }
+            MainMenu::SaveChanges=> {
+                Command::none()
+            }   
+            MainMenu::ShowLogs=>{
+                self.object = MainMenu::ShowLogs;
+                Command::none()
+            }
+        }
     }
-
+    /*
+    TODO:center everything
+         add table widget?
+     */
     fn view(&self) -> Element<'_, Self::Message> {
-        column![
+         let btn_row:Row<MainMenu,Renderer> = row![
             button("Active Tasks")
-        ]
-        .padding(10)
-        .align_items(Alignment::Center)
-        .into()
+            .style(theme::Button::Secondary)
+            .on_press(self::MainMenu::CurrentTasks)
+            ,
+            button("show logs")
+                .style(theme::Button::Secondary)
+                .on_press(self::MainMenu::ShowLogs)
+                ].padding(Padding::horizontal(55.into()));
+
+        let mut sub_view :Column<MainMenu,Renderer> = 
+            column![btn_row];
+        match self.object{
+            MainMenu::CurrentTasks => {
+                sub_view.push(text("show current tasks")).into()
+            }
+            MainMenu::ShowLogs => {
+                sub_view.push(text("show logs")).into()
+            }
+            MainMenu::SaveChanges => todo!(),
+            MainMenu::DiscardAndExit => todo!(),
+        }
+        
     }
-    
+    fn theme(&self) -> Self::Theme {
+        self::Theme::Dark
+    }
 }
+
 
 
 
