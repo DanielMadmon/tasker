d821b6c4066f4d44489e3df7dd15fc82d701f91d
taijen <danielmadmonb@gmail.com>
2023-06-17 14:36:02 +0300

    working on gui

--- bin/tasker_gui/src/main.rs
+++ bin/tasker_gui/src/main.rs
@@ -8,18 +8,18 @@ slint::slint!{
 }
 pub fn main(){ 
     let app = App::new().unwrap();
-    let mut rows:Vec<Row> = app.get_table().iter().collect();
+    let mut rows:Vec<TableRow> = app.get_table().iter().collect();
     rows.extend(rows.clone());
-    let new_row = Row{
+    let new_row = TableRow{
         name:SharedString::from("test1")
     };
     rows.push(new_row);
-    let new_row = Row{
+    let new_row = TableRow{
         name:SharedString::from("test2")
     };
     rows.push(new_row);
-    
-    let table_model: Rc<VecModel<Row>> = Rc::new(VecModel::from(rows));
+
+    let table_model: Rc<VecModel<TableRow>> = Rc::new(VecModel::from(rows));
     app.set_table(table_model.into());
     app.run().unwrap();
 }
